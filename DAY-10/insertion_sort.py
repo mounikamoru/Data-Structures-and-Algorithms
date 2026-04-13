b25df1c2f18516c1d1c23cbfe124d7a2514b9032
mounikamoru <mounikamoru4634@gmail.com>
2026-04-13 23:47:49 +0530

    Add files via upload

---
+++ DAY-10/insertion_sort.py
@@ -0,0 +1,72 @@
+'''#example of insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        #shifting elements
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+arr = [9, 4, 2, 7]
+print(insertion_sort(arr))
+
+#counting the shifts in insertion sort:
+def insertion_sort(arr):
+    count = 0
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        #shifting elements
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        count += 1
+    return arr,count
+arr = [9, 4, 2, 7]
+print(insertion_sort(arr))
+
+#inserting an element in sorted array:
+
+#arr = [1,3,5,7]
+#x = 4
+#arr = [1,3,4,5,7]
+
+approach:
+1.start from end of an array
+2.shift elements greater than x
+3.insert x in correct position
+
+def insert_into_sorted(arr, x):
+    arr.append(0)
+    i = len(arr)-2
+    #shifting elements to right
+    while i >= 0 and arr[i] > x:
+        arr[i+1] = arr[i]
+        i -= 1
+    #insert
+        arr[i+1] = x
+        return arr
+arr = [1,3,5,7]
+x = 4
+print(insert_into_sorted(arr, x))'''
+
+#code for Quick sort
+
+def quick_sort(arr):
+    if len(arr) < 1:
+        return arr
+    pivot = arr[-1]
+    left = []
+    right = []
+    for i in arr[:-1]:
+        if i < pivot:
+            left.append(i)
+        else:
+            right.append(i)
+    return quick_sort(left) + [pivot] + quick_sort(right)
+arr = [5, 2, 4, 1]
+print(quick_sort(arr))
+    
